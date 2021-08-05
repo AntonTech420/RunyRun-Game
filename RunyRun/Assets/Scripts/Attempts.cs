621202142ff4dec5989ac9183d92d50b3f8be0cf
AMW <73808213+AntonTech420@users.noreply.github.com>
2021-08-05 21:50:13 +1200

    Base game V1

--- RunyRun/Assets/Scripts/Attempts.cs
+++ RunyRun/Assets/Scripts/Attempts.cs
@@ -5,9 +5,18 @@ using UnityEngine.UI;
 using TMPro;
 
 
-public class Attempts : MonoBehaviour
-{
-    public TextMeshProUGUI AttemptText;
+// public class Attempts : MonoBehaviour
+// {
+//     public TextMeshProUGUI AttemptText;
+
+//     [SerializeField]
+// //   string strTag;
+// //     private void OnCollisionEnter(Collision collision) 
+// //     {
+// //         if (collision.collider.tag == strTag);
+            
+            
+// //     }
 
 
-}
+// }
---
+++ RunyRun/Assets/Scripts/SettingsMenu.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class SettingsMenu : MonoBehaviour
+{
+    public AudioSource AudioSource;
+
+    public Slider VolumeSlider;
+
+    private float MusicVolume =1f;
+    
+    
+   void Start() 
+    {
+        AudioSource.Play();
+        // MusicVolume = PlayerPrefs.GetFloat("volume");
+        // AudioSource.volume = MusicVolume;
+        // VolumeSlider.value = MusicVolume;
+    }
+
+    void Update()
+    {
+        AudioSource.volume = MusicVolume;
+        PlayerPrefs.SetFloat("volume", MusicVolume);
+    }
+
+    public void UpdateVolume( float volume) 
+    {
+        MusicVolume = volume;
+    }   
+}
